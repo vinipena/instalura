21c0bb1f65c2dcd35dd6d8ad87c515c65ae799c2
Vinicius Pena <vacpena@gmail.com>
2021-02-14 19:44:08 -0300

    Aula 05 - CSS Reset  - Footer - Next/Head e Fonts

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import styled from 'styled-components'
 import Menu from '../src/componentes/commons/Menu'
-
+import Footer from '../src/componentes/commons/Footer'
 export default function Home() {
   return (
-  <div>
+  <div
+  style={{
+    flex: '1',
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'column',
+    justifyContent: 'space-between',}}
+  >
     <Menu/>
+    <Footer/>
   </div>
   )
 }
