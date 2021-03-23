01525897142e71b27d8125cdec7184efa1b70a61
Vinicius Pena <vacpena@gmail.com>
2021-03-22 21:47:21 -0300

    feat: aula03 adicionando SEO

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import { lightTheme, darkTheme } from '../src/theme/index';
 import GlobalStyle from '../src/theme/GlobalStyle';
 
 import Toggle from '../src/theme/Toggle';
+import SEO from '../src/components/commons/SEO';
 
 // eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
@@ -23,17 +24,14 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Instalura - Projeto Base</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        />
+
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
       </Head>
+      <SEO headTitle="Home" />
 
       <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
         <GlobalStyle />
