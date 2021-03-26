11aca4d0a5367fc55c2ef2cb3181d401b186eaa9
Vinicius Pena <vacpena@gmail.com>
2021-03-26 00:10:47 -0300

    feat: aula07 mod03

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const redirects = require('./config/redirects');
 
 module.exports = {
-  // Configuração da barra para campanhas de marketing
+  // Configuração da barra para campanhas de marketing e URL
   trailingSlash: true,
   async redirects() {
     return redirects;
