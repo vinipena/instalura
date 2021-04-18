31bc19ab2ecc3e6feb4cbdf0d54afd593eb1cfb2
Vinicius Pena <vacpena@gmail.com>
2021-04-17 21:15:54 -0300

    feat: aula02mod04 loginservice e testes

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -3,19 +3,33 @@
 describe('/pages/app/login/', () => {
   //  it === test a ser feito
   it('Preencha os campos e vá para a pagina /app/profile', () => {
+    cy.intercept('https://instalura-api.vercel.app/api/login')
+      .as('userLogin');
+
     cy.visit('/app/login/');
 
     // document.querySelector('input[name="usuario"]')
     // preencher o input de usuario
-    cy.get('#formCadastro input[name="usuario"]').type('usuario');
+    cy.get('#formCadastro input[name="usuario"]').type('omariosouto');
 
     // preencher o input de senha
-    cy.get('#formCadastro input[name="senha"]').type('senha');
+    cy.get('#formCadastro input[name="senha"]').type('senhasegura');
 
     // clicar no botao submit
     cy.get('#formCadastro button[type="submit"]').click();
 
     // o que esperamos? ir para "/app/profile"
     cy.url().should('include', '/app/profile');
+
+    // Temos o token?
+    cy.wait('@userLogin')
+      .then((intercept) => {
+        const { token } = intercept.response.body.data;
+
+        cy.getCookie('APP_TOKEN')
+          .should('exist')
+          // token do cookie é igual ao do server?
+          .should('have.property', 'value', token);
+      });
   });
 });
