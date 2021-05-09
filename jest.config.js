d01b9035a94da90874531de9f07ef1a7da3bb2b5
vinipena <vacpena@gmail.com>
2021-05-09 18:42:08 -0300

    feat: React testing lib

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  verbose: true,
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  setupFilesAfterEnv: [
+    '<rootDir>/jest.setup.js',
+  ],
+  moduleDirectories: [
+    '<rootDir>/node_modules',
+    'node_modules',
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/cypress/',
+    '<rootDir>/dist/',
+  ],
+};
